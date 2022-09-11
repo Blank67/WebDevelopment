dc15ee5593022184495f32d29376d823fbc291af
Blank67 <hmnshshrm67@gmail.com>
2022-09-11 21:25:07 +0530

    task 3 done

--- DOMtask3/dom.js
+++ DOMtask3/dom.js
@@ -15,3 +15,35 @@
 // console.log(document.links);
 // console.log(document.images);
 
+// //GET ELEMENT BY ID
+// console.log(document.getElementById('header-title'));
+// let headerTitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header');
+// console.log(headerTitle);
+// // headerTitle.textContent = 'Hello!'
+// // headerTitle.innerText = 'GoodBye!'
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello!</h3>';
+// // headerTitle.style.borderBottom = 'solid 3px #000';
+// header.style.borderBottom = 'solid 3px #000';
+
+// //GET ELEMENT BY CLASS
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2!';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+// //items.style.backgroundColor = '#f4f4f4'; //We can't change color of all the items, to do that we need to loop through it.
+// for(let i=0;i<items.length;i++){ //To change color of all
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+
+//TASK 3
+let header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+let titleClass = document.getElementsByClassName('title');
+titleClass[0].style.fontWeight = 'bold';
+titleClass[0].style.color = 'green';
