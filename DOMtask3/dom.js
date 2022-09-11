3d801315543fa51fb4ab55da34080559014a930c
Blank67 <hmnshshrm67@gmail.com>
2022-09-11 20:50:15 +0530

    Task 3

---
+++ DOMtask3/dom.js
@@ -0,0 +1,17 @@
+//EXAMINE THE DOCUMENT OBJECT
+// console.dir(document);
+// console.log(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 'Changed'; //We can also change document object
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
